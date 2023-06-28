a8980cf0ee370d3022d02ef0303e7965a28caa02
DariusKaseta <kasetadarius@gmail.com>
2023-06-28 14:46:30 +0300

    models and first migration

--- ptu12_final/create_scrape_send/admin.py
+++ ptu12_final/create_scrape_send/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
+from .models import PersonalInformation, AboutMe
+from django import forms
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 # Register your models here.
+
+admin.site.register(models.AboutMe)
+
+
+class PersonalInformationForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            'phone_number': PhoneNumberPrefixWidget(initial='LT'),
+        }
+
+@admin.register(PersonalInformation)
+class PersonalInformationAdmin(admin.ModelAdmin):
+    form = PersonalInformationForm
--- ptu12_final/ptu12_final/settings.py
+++ ptu12_final/ptu12_final/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "create_scrape_send"
+    "create_scrape_send",
+    "phonenumber_field",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PHONENUMBER_DEFAULT_REGION = "LT"
